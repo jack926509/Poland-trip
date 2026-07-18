b0d159f1a6b708fc67c1451a060f80eaa2e7b02b
jack926509 <jack926509@email.com>
2026-07-18 23:28:46 +0800

    feat: 固定手機 PWA 啟動與舊入口導向

--- legacy-redirect.js
+++ legacy-redirect.js
@@ -1,6 +1,10 @@
 (function () {
-  const target = new URL('./', window.location.href);
-  target.search = window.location.search;
+  const filename = window.location.pathname.split('/').pop();
+  const desktop = filename === 'desktop.html';
+  const target = new URL(desktop ? './' : './mobile.html', window.location.href);
+  const searchParams = new URLSearchParams(window.location.search);
+  searchParams.set('view', desktop ? 'desktop' : 'mobile');
+  target.search = searchParams.toString();
   target.hash = window.location.hash;
   window.location.replace(target.href);
 })();
